906c42ac4db2b1e708fbcadcde0915bcd1b46fa7
Aditya Shrivastava <ibelong2u+github@gmail.com>
2021-02-19 08:50:30 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,34 @@
+// settings about your website!
+
+var city_name = "College Station Chapter";
+
+var register = "To sign up for our weekly newsletter, <a href='http://groups.google.com/group/aoltamu' target='_blank'>Click Here</a>";
+
+var calendar = "Upcoming Part-1 course on Sept 26-30";
+
+var contact = " Call us on 979-422-0597 <br/> E-Mail: artofliving@tamu.edu";
+
+var center1 = "Come be a part of one of the largest volunteer based Non-Governmental Organization in the world. Our service projects, programs on yoga, meditation and stress elimination have benefited over 300 million people.";
+var center2 = "The growing demands placed on an individual to be successful in our fast-paced, stressful global society make it even more necessary for us to have the tools we need to make responsible life decisions with confidence and clarity, more joy and enthusiasm while developing a broader vision along with cultivating leadership potential.";
+var center3 = "Quality and success in life are determined by our state of mind. Under stress and pressure, the mind often vacillates between regretting the past and worrying about the future. This constant vacillation results in lower performance, lack of concentration, decreased efficiency, and more interpersonal conflicts.";
+var center4 = "Our programs both challenges and empowers students to become more centered and focused, to manage negative emotions and to live life more fully in the present moment, with increased awareness, perception and expression. Graduates of the course report improved focus and concentration, increased confidence, enhanced creativity and efficiency throughout the day as well as improved interpersonal relationships and a sense of naturalness in life.";
+
+var left1_title = "The Absolute Yoga";
+var left1_heading = "asanas, meditation, wisdom";
+var left1_content = "Starting this Fall";
+
+var left2_title = "Full Moon Meditation";
+var left2_heading = "August 29";
+var left2_content = "Guided Meditation open for all";
+
+var left3 = "put any information here or leave blank";
+
+var right1_title = "Part-1 course";
+var right1_heading = "August 8";
+var right1_content = "<i>Stay calm when the pressure is on! (<a href='http://ibelong2u.googlepages.com/handout.doc' target='_blank'>more</a>)</i>" +  "<div id='enroll'> <b>Course fee: $375</b><br><span style='font-size:x-small;'>(students/senior citizens -$200)</span><br/>" + "<!-- <input type='button' value='Enroll Now' onclick='javascript:enroll();' /> -->";
+
+var right2_title = "Knowledge Nights";
+var right2_heading = "6pm on Saturdays";
+var right2_content = "Practical Wisdom from the timeless wisdom of yoga";
+
+var right3 = "photos";
